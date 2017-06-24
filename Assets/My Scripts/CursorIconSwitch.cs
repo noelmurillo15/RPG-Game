615097783ee0f44879ad80d720f5fea0387f02f2
Allan Murillo <noelmurillo15@fullsail.edu>
2017-06-24 12:47:21 -0400

    Gamepad Movement
    Functional! Press G to swap between Mouse Movement

---
+++ Assets/My Scripts/CursorIconSwitch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorIconSwitch : MonoBehaviour {
+
+    CameraRaycaster camRaycaster;
+    [SerializeField] Texture2D walkCursor = null;
+    [SerializeField] Texture2D attackCursor = null;
+    [SerializeField] Texture2D whatCursor = null;
+    [SerializeField] Vector2 cursorHotspot = new Vector2(96,96);
+
+    // Use this for initialization
+    void Start () {
+        camRaycaster = GetComponent<CameraRaycaster>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        switch (camRaycaster.layerHit)
+        {
+            case Layer.Enemy:
+                Cursor.SetCursor(attackCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case Layer.Walkable:
+                Cursor.SetCursor(walkCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            default:
+                Cursor.SetCursor(whatCursor, cursorHotspot, CursorMode.Auto);
+                break;
+        }
+	}
+}
---
+++ Assets/My Scripts/PlayerMovement.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof(ThirdPersonCharacter))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float walkMoveStopRadius = 0.2f;
+    ThirdPersonCharacter m_Character;
+    private CameraRaycaster cameraraycaster;
+    Vector3 currentClickTarget;
+    bool InputType = false;    //  TODO consider making static layer
+
+    // Use this for initialization
+    void Start()
+    {
+        cameraraycaster = Camera.main.GetComponent<CameraRaycaster>();
+        m_Character = GetComponent<ThirdPersonCharacter>();
+        currentClickTarget = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+            InputType = !InputType;
+
+        if (InputType)
+            MouseMovement();
+        else
+            GamepadMovement();
+    }
+
+    private void GamepadMovement()
+    {
+
+    //  Joystick Movement
+        // read inputs
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // calculate camera relative direction to move:
+        Vector3 m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 m_Move = v * m_CamForward + h * Camera.main.transform.right;
+
+        m_Character.Move(m_Move, false, false);
+    //  Joystick Movement
+    }
+
+    private void MouseMovement()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            print("Cursor raycast hit layer: " + cameraraycaster.layerHit);
+            switch (cameraraycaster.layerHit)
+            {
+                case Layer.Enemy:
+                    print("Clicked on Enemy");
+                    break;
+                case Layer.Walkable:
+                    currentClickTarget = cameraraycaster.hit.point;
+                    m_Character.Move(currentClickTarget - transform.position, false, false);
+                    break;
+
+
+                default:
+                    print("Something is missing a layer");
+                    break;
+            }
+
+            var playerToClickPoint = currentClickTarget - transform.position;
+            if (playerToClickPoint.magnitude >= walkMoveStopRadius)
+            {
+                m_Character.Move(playerToClickPoint, false, false);
+            }
+            else
+            {
+                m_Character.Move(Vector3.zero, false, false);
+            }
+        }
+    }
+}
