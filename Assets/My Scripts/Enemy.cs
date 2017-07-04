bb4f77938d9baef294cc3e6635defeeec0b2e1ca
Allan Murillo <noelmurillo15@fullsail.edu>
2017-07-04 17:11:27 -0400

    Enemy HP
    Floating enemy health bars

---
+++ Assets/My Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] float MaxHP = 100f;
+    [SerializeField] float currentHP = 100f;
+
+    public float healthAsPercentage
+    {
+        get
+        {
+            return currentHP / MaxHP;
+        }
+    }
+}
