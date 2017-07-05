4fbe16eac5a73f6a45be9d4e0550eb6e3e378af6
Allan Murillo <noelmurillo15@fullsail.edu>
2017-07-04 21:19:15 -0400

    Learning Enemy Animation
    Learning how to make a animator Controller for my enemy

--- Assets/RPG Tutorial/Enemy/Enemy.cs
+++ Assets/RPG Tutorial/Enemy/Enemy.cs
@@ -1,17 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Enemy : MonoBehaviour {
 
     [SerializeField] float MaxHP = 100f;
-    [SerializeField] float currentHP = 100f;
+    [SerializeField] float attackRadius;
+
+    AICharacterControl aiCharacterControl = null;
+    GameObject player = null;
+    Animator anim = null;
+
+    float currentHP = 100f;
+    int speedHash = Animator.StringToHash("Speed");
 
     public float healthAsPercentage
     {
         get
         {
             return currentHP / MaxHP;
+        }
+    }
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        aiCharacterControl = GetComponent<AICharacterControl>();
+    }
+
+    void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+
+        if(distanceToPlayer <= attackRadius)
+        {
+            aiCharacterControl.SetTarget(player.transform);
+            anim.SetFloat(speedHash, distanceToPlayer);
+        }
+        else
+        {
+            aiCharacterControl.SetTarget(transform);
         }
     }
 }
